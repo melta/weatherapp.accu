d05332e39355be0cd58ee5b989017979cb2696c1
Taras Melnychuk <melnychuktaras@gmail.com>
2019-01-20 12:18:47 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='weatherapp.accu',
+    version='0.1.0',
+    author='Taras Melnychuk',
+    description="AccuWeather provider",
+    long_descriptoin="",
+    packages=find_namespace_packages(),
+    entry_points={
+        'weatherapp.provider': 'accu=weatherapp.accu.provider:AccuProvider',
+    },
+    install_requires=[
+        'requests',
+        'bs4'
+    ]
+)
