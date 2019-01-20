81ba209c3c480b102e94c0838bd73e420abeb002
Taras Melnychuk <melnychuktaras@gmail.com>
2019-01-20 12:40:48 +0200

    Add accu related configuration

---
+++ weatherapp/accu/config.py
@@ -0,0 +1,7 @@
+ACCU_PROVIDER_NAME = 'accu'          # provider id
+ACCU_PROVIDER_TITLE = 'AccuWeather'  # provider title
+
+DEFAULT_ACCU_LOCATION_NAME = 'Lviv'
+DEFAULT_ACCU_LOCATION_URL = \
+    ('https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561')
+ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
